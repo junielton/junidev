802918893a2d528e94c449d01e86ff6d07b94ce2
junielton <junyellton@gmail.com>
2022-03-21 14:35:50 -0300

    add a object default value to create context

--- context/ThemeContextProvider.tsx
+++ context/ThemeContextProvider.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useState } from 'react'
 
-const ThemeContext = createContext()
+const ThemeContext = createContext({})
 
 export default function ThemeContextProvider({ children }) {
 	const themes = {
@@ -8,14 +8,14 @@ export default function ThemeContextProvider({ children }) {
 			color: '#f7f6ee',
 			background: '#263238',
 			inverseColor: '#263238',
-			inverseBackground: '#f7f6ee'
+			inverseBackground: '#f7f6ee',
 		},
 		light: {
 			color: '#263238',
 			background: '#f7f6ee',
 			inverseColor: '#f7f6ee',
-			inverseBackground: '#263238'
-		}
+			inverseBackground: '#263238',
+		},
 	}
 	const [theme, setTheme] = useState(themes.light)
 
@@ -31,4 +31,3 @@ export function useTheme() {
 	const { themes, theme, setTheme } = context as any
 	return { themes, theme, setTheme }
 }
-
